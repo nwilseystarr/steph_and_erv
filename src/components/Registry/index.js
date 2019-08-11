a2e52d6696b8fef42e1707522bae8c59dbcb4eb5
Nicole Wilsey Starr <Nicole.WilseyStarr@gmail.com>
2019-08-11 18:28:17 -0500

    changed logo and font color

---
+++ src/components/Registry/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class Registry extends Component {
+    render(){
+        return(
+            <div className="container">
+                {/*Col One*/}
+                {/*Col Two*/}
+                {/*Col Three*/}                                
+            </div>
+        )
+    }
+}
